f1fa89a7d6fe7e4de4a7180d2198e97da4d834a5
madeline-park2 <madelinepark@ymail.com>
2025-01-12 17:05:33 -0800

    color and stuff

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
@@ -34,6 +37,10 @@ fun GradScreen(modifier: Modifier = Modifier) {
     Text(
         text = "Graduation Announcement",
         fontSize = 50.sp,
+        lineHeight = 50.sp,
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
         modifier = modifier
     )
 }
